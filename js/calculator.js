b38f773d99291c593e3769742cbcfe39063a6603
Vector40 <40523265+lnnocentchisanga3@users.noreply.github.com>
2021-04-21 20:05:55 +0200

    some more changes on mmplanner

--- js/calculator.js
+++ js/calculator.js
@@ -31,7 +31,8 @@
 document.querySelector('#amountPlan').addEventListener('keydown', function(e){
 			if(e.code === "Enter"){
 				planMoney();
-				console.log("clicked");			}
+				displayData();
+				}
 		});
 })();
 
@@ -39,8 +40,9 @@ document.querySelector('#amountPlan').addEventListener('keydown', function(e){
 
 
 function planMoney() {
+	displayData();
 	var money = document.getElementById('amountPlan').value;
-	if (money == "!@#$%&*{}+_)("){
+	if (money.includes("!@#$%&*{}+_)(") || money == 0 || money == ""){
 		document.getElementById('show').innerHTML = "Enter the Valid Money";
 	}else{
 	document.getElementById('amountPlan').value = null;
@@ -53,9 +55,9 @@ function planMoney() {
 
 	if (fans == 0.1 || fans == 0.2 || fans == 0.3 || fans == 0.4 || fans == 0.5 || fans == 0.6 || fans == 0.7 || fans == 0.8 || fans == 0.9) {
 		var convert = fans*10;
-		document.getElementById('showMe').innerHTML = convert+" ngwe";
+		document.getElementById('showMe').innerHTML = "<i class='fa fa-refresh'></i>"+" "+"Disaster account: <span class='float-right'>"+convert+" ngwe</span>";
 	}else{
-		document.getElementById('showMe').innerHTML = "Disaster account: K "+fans;
+		document.getElementById('showMe').innerHTML = "<i class='fa fa-refresh'></i>"+" "+"Disaster account: <span class='float-right'>K "+fans+"</span>";
 	}
 
 //Business account
@@ -66,9 +68,9 @@ var ansBus = 60/100;
 
 	if (fansBus == 0.1 || fansBus == 0.2 || fansBus == 0.3 || fansBus == 0.4 || fansBus == 0.5 || fansBus == 0.6 || fansBus == 0.7 || fansBus == 0.8 || fansBus == 0.9) {
 		var convertBus = fansBus*10;
-		document.getElementById('showBus').innerHTML = convertBus+" ngwe";
+		document.getElementById('showBus').innerHTML = "<i class='fa fa-money'></i>"+" "+"Business Account: <span class='float-right'>"+convertBus+" ngwe</span>";
 	}else{
-		document.getElementById('showBus').innerHTML = "Business Account: K "+fansBus;
+		document.getElementById('showBus').innerHTML = "<i class='fa fa-money'></i>"+" "+"Business Account: <span class='float-right'>K "+fansBus+"</span>";
 	}
 
 //investment account
@@ -78,9 +80,9 @@ var investment = 10/100;
 
 	if (investmentf == 0.1 || investmentf == 0.2 || investmentf == 0.3 || investmentf == 0.4 || investmentf == 0.5 || investmentf == 0.6 || investmentf == 0.7 || investmentf == 0.8 || investmentf == 0.9) {
 		var convertInvestment = investmentf*10;
-		document.getElementById('showInvestment').innerHTML = convertInvestment+" ngwe";
+		document.getElementById('showInvestment').innerHTML = "<i class='fa fa-dollar'></i>"+" "+"Investment Account: <span class='float-right'>"+convertInvestment+" ngwe</span>";
 	}else{
-		document.getElementById('showInvestment').innerHTML = "Investment Account: K "+investmentf;
+		document.getElementById('showInvestment').innerHTML = "<i class='fa fa-dollar'></i>"+" "+"Investment Account: <span class='float-right'>K "+investmentf+"</span>";
 	}
 
 //Bills/ Expense account
@@ -91,9 +93,9 @@ var bills = 10/100;
 
 	if (fbills == 0.1 || fbills == 0.2 || fbills == 0.3 || fbills == 0.4 || fbills == 0.5 || fbills == 0.6 || fbills == 0.7 || fbills == 0.8 || fbills == 0.9) {
 		var convertBills = fbills*10;
-		document.getElementById('showBills').innerHTML = convertBills+" ngwe";
+		document.getElementById('showBills').innerHTML = "<i class='fa fa-credit-card'></i>"+" "+"Expense Account: <span class='float-right'>"+convertBills+" ngwe</span>";
 	}else{
-		document.getElementById('showBills').innerHTML = "Expense Account: K "+fbills;
+		document.getElementById('showBills').innerHTML ="<i class='fa fa-credit-card'></i>"+" "+"Expense Account: <span class='float-right'>K "+fbills+"</span>";
 	}
 
 
@@ -105,9 +107,9 @@ var leisure = 5/100;
 
 	if (leisuref == 0.1 || leisuref == 0.2 || leisuref == 0.3 || leisuref == 0.4 || leisuref == 0.5 || leisuref == 0.6 || leisuref == 0.7 || leisuref == 0.8 || leisuref == 0.9) {
 		var convertleisure = leisuref*10;
-		document.getElementById('showleisure').innerHTML = convertleisure+" ngwe";
+		document.getElementById('showleisure').innerHTML = "<i class='fa fa-home'></i>"+" "+"Vacation Account: <span class='float-right'>"+convertleisure+" ngwe</span> ";
 	}else{
-		document.getElementById('showleisure').innerHTML = "Vacation Account: K "+leisuref;
+		document.getElementById('showleisure').innerHTML = "<i class='fa fa-home'></i>"+" "+"Vacation Account: <span class='float-right'>K "+leisuref+"</span>";
 	}
 }
 
@@ -119,12 +121,17 @@ var extra = 5/100;
 
 	if (extraCash == 0.1 || extraCash == 0.2 || extraCash == 0.3 || extraCash == 0.4 || extraCash == 0.5 || extraCash == 0.6 || extraCash == 0.7 || extraCash == 0.8 || extraCash == 0.9) {
 		var convertExtra = extraCash*10;
-		document.getElementById('showExtra').innerHTML = convertExtra+" ngwe";
+		document.getElementById('showExtra').innerHTML = "<i class='fa fa-plus'></i>"+" "+"Extra Account: <span class='float-right'>"+convertExtra+" ngwe</span>";
 	}else{
-		document.getElementById('showExtra').innerHTML = "Extra Account: K "+extraCash;
+		document.getElementById('showExtra').innerHTML = "<i class='fa fa-plus'></i>"+" "+"Extra Account: <span class='float-right'>K "+extraCash+"</span>";
 	}
 }
 
 
-
+function disData() {
+	document.getElementById('displayData').style.display = "none";
+}
+function displayData() {
+	document.getElementById('displayData').style.display = "flex";
+}
 
